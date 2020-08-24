19410c26c540940593b41c8a31f4a7a16bd4f191
Tessakam <tessa.kam@hotmail.com>
2020-08-24 15:28:04 +0200

    04 done

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -9,8 +9,16 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+        var Age = prompt("How old are you?");
+        var Gender = prompt("What is your gender?", "Male or Female");
+        var Town = prompt("Where do you live?");
 
-    // your code here
-
-})();
+    if (Confirm ("You are " + Age + "years old" + Gender + "and lives in" + Town) == true) {
+        alert("Thank you for confirming");
+    }
+    else if {
+    (Confirm == false)
+        alert("Please confirm")
+    }
+)
