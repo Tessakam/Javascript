5063fdf1d2ed93435c07e66eaa6c283c8b2f3071
Tessakam <tessa.kam@hotmail.com>
2020-08-25 15:45:46 +0200

    06 done

---
+++ 02-maths/06-fizzbuzz/script.js
@@ -0,0 +1,30 @@
+/* becode/javascript
+ *
+ * /02-maths/06-fizzbuzz/script.js - 2.6: fizzbuzz
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+//Example: https://stackoverflow.com/questions/13845437/from-1-to-100-print-ping-if-multiple-of-3-pong-if-multiple-of-5-or-else-p//
+(function() {
+
+    for (var x = 1; x <= 100; x++) {
+
+        if ((x % 3 === 0) && (x % 5 === 0)) {
+            console.log("fizzbuzz");
+
+        } else if (x % 3 === 0) {
+            console.log("fizz = multiple of 3");
+
+        } else if (x % 5 === 0) {
+            console.log("buzz = multiple of 5");
+
+        } else {
+            console.log(x);
+        }
+    }
+})();
