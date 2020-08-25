1cc853ccdc73b526c9897e8f194060d1658a1b21
Tessakam <tessa.kam@hotmail.com>
2020-08-25 11:24:11 +0200

    03 done // include loop

---
+++ 02-maths/03-even-square/script.js
@@ -0,0 +1,26 @@
+/* becode/javascript
+ *
+ * /02-maths/03-even-square/script.js - 2.3: carrés des pairs
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+        var msg = "";
+        var sum = 0;
+
+        for (var x = 1; x <= 21; x++) {
+            var square = x * x;
+            var msg  = msg + square + "\n";
+        }
+        alert(msg);
+
+    });
+
+})();
