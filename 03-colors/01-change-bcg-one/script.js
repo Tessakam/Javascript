fd7e3273ebed0ee636b887600db4a25bff7ed26f
Tessakam <tessa.kam@hotmail.com>
2020-08-25 16:33:11 +0200

    01 done

---
+++ 03-colors/01-change-bcg-one/script.js
@@ -0,0 +1,31 @@
+/* becode/javascript
+ *
+ * /03-colors/01-change-bcg-one/script.js - 3.1: couleur de fond (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+//Example: https://stackoverflow.com/questions/197748/how-do-i-change-the-background-color-with-javascript
+
+(function() {
+    red.addEventListener('click', function () {
+        document.body.style.background = "red";
+    });
+
+    green.addEventListener('click', function () {
+        document.body.style.background = "green";
+    });
+
+    yellow.addEventListener('click', function () {
+        document.body.style.background = "yellow";
+    });
+
+    blue.addEventListener('click', function () {
+        document.body.style.background = "blue";
+    });
+
+})();
