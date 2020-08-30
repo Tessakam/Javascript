b9b6c967538fcb198529878b37de0d5364afbce9
Tessakam <tessa.kam@hotmail.com>
2020-08-30 18:42:29 +0200

    review

--- 05-arrays/01-get-element/script.js
+++ 05-arrays/01-get-element/script.js
@@ -9,11 +9,11 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     run.addEventListener('click', function () {
 
-        var fruits = [
+        let fruits = [
             "apple",
             "pear",
             "raspberry",
