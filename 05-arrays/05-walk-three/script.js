dc9b02fc52d57790f6a8ff12fff5d32dab6db68e
Tessakam <tessa.kam@hotmail.com>
2020-08-30 18:55:11 +0200

    forEach with filter

--- 05-arrays/05-walk-three/script.js
+++ 05-arrays/05-walk-three/script.js
@@ -14,7 +14,7 @@
 
     run.addEventListener('click', function () {
 
-        var people = [
+        let people = [
             {
                 firstname: "Garald",
                 lastname: "Freschi",
