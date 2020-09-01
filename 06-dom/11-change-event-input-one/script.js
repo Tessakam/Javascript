79cb6ef0973b1577735d6cc2c5cade709e05cb38
Tessakam <tessa.kam@hotmail.com>
2020-09-01 15:39:44 +0200

    input password with max 10 characters / redo with Awet

--- 06-dom/11-change-event-input-one/script.js
+++ 06-dom/11-change-event-input-one/script.js
@@ -9,22 +9,39 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function () {
+(function () { // did the exercise again with Awet
 
-    function password() {
+    let x = 1;
+    let input = document.getElementById("pass-one");
+    let counter = document.getElementById("counter");
+    input.addEventListener("keypress",  () => { // => same as function // arrow function
+
+        if (x < 11){
+        counter.innerHTML = `${x}/10` //add up the input in counter - using text template ``
+        x++}
+
+        else {
+        input.setAttribute("maxlength", "10");
+    }
+        console.log("I want to display" + x) // using text template `` is the same but shorter
+})
+})
+
+    ();
+
+    /*  original code
+
+    function password(event) {
 
         let input = document.getElementById("pass-one");
         input.maxLength = 10;
 
         let span = document.getElementById("pass-one").value.length;
-         document.getElementById("counter").innerHTML = span + '/10';
+        document.getElementById("counter").innerHTML = span + '/10';
         //result.innerHTML = input.value
     }
 
-    document.getElementById("pass-one").addEventListener("onkeypress", password)
+    document.getElementById("pass-one").addEventListener("onkeypress",password);
     //onkeypress, otherwise nothing happens
     //console.log("Maximum allowed characters in the password is 10.")
-
-})();
-
-
+*/
