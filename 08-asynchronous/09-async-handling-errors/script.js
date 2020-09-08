05b0b3825a70853bec3ab58876452f0ae57d48bc
Tessakam <tessa.kam@hotmail.com>
2020-09-08 15:39:34 +0200

    8.9 Use async with try and catch - result same as 8.6

---
+++ 08-asynchronous/09-async-handling-errors/script.js
@@ -0,0 +1,24 @@
+/* becode/javascript
+ *
+ * /10-asynchronous/09-async-handling-errors/script.js - 10.9: gestion d'erreur (async/await)
+ *
+ * coded by leny@BeCode
+ * started at 09/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    document.getElementById("run").addEventListener("click", persons)
+
+    async function persons() {
+        try {
+            let result = await window.lib.getPersons();
+            console.log (result)
+        }
+        catch (error) {
+            console.log (error)
+        }
+    }
+})();
