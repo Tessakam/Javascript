db75c92eac08bf8c61c0efbbb717c06877eb747a
Tessakam <tessa.kam@hotmail.com>
2020-09-10 12:33:58 +0200

    functions created - still need to activate the buttons

--- js-games/00.Rock-scissors-paper/script.js
+++ js-games/00.Rock-scissors-paper/script.js
@@ -13,8 +13,26 @@
 // 3) Compare the 2 choices and determine the winner
 // 4) Start program and dispay results
 
+// Requirements
+// [x] Have three buttons that the player can press to change their pick
+// [x] Have another button to let the computer pick one and show the winner
+// [ ] Let the player know they won (no alert box)
+// [x] Have a reset button
+
+// Nice-to-have
+// [x] Nice graphics
+// [x] Let the player input using a text field
+// [ ] Show what the player picked by highlighting the correct button
+// [ ] Add more options (spock, ...)
+// [ ] Add even more options (but avoid using 100 if statements)
+// [ ] Add complex rules
 
 (() => {
+    //assign all buttons
+    document.getElementById("rock")
+    let myChoiceButton = document.getElementById('rock').innerHTML
+    let myChoiceInput = document.getElementById('rock').innerHTML
+    
 
     function getUserChoice(userInput) {
 
@@ -27,23 +45,49 @@
             return console.log("Error!\nPlease use only 'rock, paper or scissors'");
         }
     }
-    console.log(getUserChoice("paper"))
-    console.log(getUserChoice("test"))
+    //console.log(getUserChoice("test"))
 
+    function getComputerChoice() {
 
-    function getComputerChoice () {
         let randomNumber = Math.floor(Math.random() * 3);
-
-        switch(randomNumber){
+        switch (randomNumber) {
             case "0":
                 return "rock";
             case "1":
                 return "paper";
             case "2":
                 return "scissors";
+            default:
+                console.log("invalid")
         }
-          }
-          console.log(getComputerChoice)
+
+        function compare(getUserChoice, getComputerChoice) {
+            if (getUserChoice === getComputerChoice) {
+                return "It's a draw, play again!"
+
+            } else if (getUserChoice === "rock") {
+                if (getComputerChoice === "scissors") {
+                    return "Congrats, you win!"
+                } else if (getComputerChoice === "paper") {
+                    return "Too bad, you lose!";
+                }
+            } else if (getUserChoice === "paper") {
+                if (getComputerChoice === "scissors") {
+                    return "Congrats, you win!"
+                } else if (getComputerChoice === "rock") {
+                    return "Congrats, you win!";
+                }
+            } else if (getUserChoice === "scissors") {
+                if (getComputerChoice === "paper") {
+                    return "Congrats, you win!"
+                } else if (getComputerChoice === "rock") {
+                    return "Too bad, you lose!";
+                }
+            }
+        }
+    }
+    /*reset bottom
+    document.getElementById("reset").reset();*/
 
 })
 ();
