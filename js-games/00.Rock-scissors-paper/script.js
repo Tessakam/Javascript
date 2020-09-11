e70c65cbdedc39d3a9f0fcaf75390cafe0d98eb6
Tessakam <tessa.kam@hotmail.com>
2020-09-11 14:34:30 +0200

    first result with minor issues to solve

--- js-games/00.Rock-scissors-paper/script.js
+++ js-games/00.Rock-scissors-paper/script.js
@@ -1,16 +1,131 @@
 // Requirements
 // [x] Have three buttons that the player can press to change their pick
 // [x] Have another button to let the computer pick one and show the winner
-// [ ] Let the player know they won (no alert box)
+// [x] Let the player know they won (no alert box)
 // [x] Have a reset button
 
 // Nice-to-have
-// [x] Nice graphics
-// [x] Let the player input using a text field
+// [ ] Nice graphics
+// [ ] Let the player input using a text field
 // [ ] Show what the player picked by highlighting the correct button
 // [ ] Add more options (spock, ...)
 // [ ] Add even more options (but avoid using 100 if statements)
 // [ ] Add complex rules
+
+(() => {
+
+    let computerSelection = "";
+    let result = "Tessa";
+    //output scores to the DOM
+    let playerScore = 0;
+    let computerScore = 0;
+    let playerSelection = 0;
+
+    document.getElementById("rock").addEventListener("click", () => {
+        playerSelection = "rock";
+        playGame()
+    });
+    document.getElementById("paper").addEventListener("click", () => {
+        playerSelection = "paper";
+        playGame()
+    });
+    document.getElementById("scissors").addEventListener("click", () => {
+        playerSelection = "scissors";
+        playGame()
+    });
+
+    let score = document.getElementById("score");
+    let scoreboard = document.getElementById("scoreboard");
+
+    function playGame() {
+         //setup a random number by computer
+
+        let computerRandom = Math.floor(Math.random());
+
+        if (computerRandom < 0.34) {
+            computerSelection = "rock";
+        } else if (computerRandom <= 0.67) {
+            computerSelection = "paper";
+        } else {
+            computerSelection = "scissors";
+        }
+        console.log(computerSelection);
+        checkWinner();
+
+        //setup a function to compare winners and return result
+
+        console.log(result);
+        if (checkWinner() === "player") {
+            result = " wins!";
+            playerScore++; //update score
+        }
+
+        else if (checkWinner() === "computer") {
+            result = " wins!";
+            computerScore++;
+        }
+
+        else {
+            result = "It's a draw!"
+        }
+
+
+        //output score into div score and scoreboard
+        score.innerHTML = "player: [ " + computerScore + " ] computer: [ " + computerScore + " ]";
+
+        //output player and computer's selections
+        scoreboard.innerHTML = "Player: [ " + playerSelection + " ] computer: [ " + computerSelection + " ]" + result
+    }
+
+    //Determine the winner
+    function checkWinner(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return "draw!"
+
+        } else if (playerSelection === "rock") {
+            if (computerSelection === "scissors") {
+                return "player"
+            } else if (computerSelection === "paper") {
+                return "computer";
+            }
+        } else if (playerSelection === "paper") {
+            if (computerSelection === "scissors") {
+                return "player!"
+            } else if (computerSelection === "rock") {
+                return "player";
+            }
+        } else if (playerSelection === "scissors") {
+            if (computerSelection === "paper") {
+                return "player!"
+            } else if (computerSelection === "rock") {
+                return "computer";
+            }
+            console.log(checkWinner)
+        }
+    }
+})
+();
+
+/*//input textfield
+let userInput = document.getElementById("input").innerText
+
+function getUserInput() {
+
+    //cover rock and Rock
+    userInput = userInput.toLowerCase();
+
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+        return userInput;
+    } else {
+        return prompt("Error!\nPlease use only 'rock, paper or scissors'");
+    }
+    //console.log(getUserInput("test"))
+}*/
+
+/*reset bottom
+document.getElementById("reset").reset();*/
+
+/* original code, where I got stuck
 
 (() => {
     //user clicks on the button + shows in text description
@@ -41,7 +156,6 @@
     }
 
     function getComputerChoice() {
-
         let randomNumber = Math.floor(Math.random() * 3);
         switch (randomNumber) {
             case "0":
@@ -80,15 +194,4 @@
             }
         }
     }
-
-    /*reset bottom
-    document.getElementById("reset").reset();*/
-
-    //you win: popup congrats
-    $(document).ready(function () {
-        $('.image-link').magnificPopup({type: 'image'});
-    });
-
-})
-();
-
+ */
